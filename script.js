17555bc3280680c73ba84c61258e47c1666cb5f6
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-04 23:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+let selectedval;
+
+function handleSubmit() {
+  selectedval = document.getElementById("colorSelect").value;
+  let select = document.querySelectorAll(".selectVal");
+  for (let i = 0; i < select.length; i++) {
+    if (select[i].value === selectedval) {
+      select[i].classList.remove("unhide");
+      select[i].classList.add("hide");
+    } else {
+      select[i].classList.remove("hide");
+      select[i].classList.add("unhide");
+    }
+  }
+  console.log(select);
+
+  console.log(selectedval, "selectedval");
+}
