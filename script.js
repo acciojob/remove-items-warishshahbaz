120f914d4b97d313a52477f887574b4eac51dcae
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-11 23:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,8 @@
 let selectedval;
 
 function handleSubmit() {
-  selectedval = document.getElementById("colorSelect").value;
-  let select = document.querySelectorAll(".selectVal");
-  for (let i = 0; i < select.length; i++) {
-    if (select[i].value === selectedval) {
-      select[i].classList.remove("unhide");
-      select[i].classList.add("hide");
-    } else {
-      select[i].classList.remove("hide");
-      select[i].classList.add("unhide");
-    }
-  }
+  
+	 var x = document.getElementById("colorSelect");
+            x.remove(x.selectedIndex);
  
 }
